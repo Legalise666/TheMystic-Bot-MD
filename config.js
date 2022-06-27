8ac9a205e2f29496884174da8d966f570517b848
Legalise666 <106928812+Legalise666@users.noreply.github.com>
2022-06-27 16:55:28 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ import fs from 'fs'
 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['5219991402134', '🤖 Mystic - Bot 🤖', true],
-  ['5219996125657']
+  ['522221035602', '👑 KHILLAZ DIOS 👑', true],
+  ['522221035602', '🤖 KHILLAZ GOD 🤖', true],
+  ['522221035602']
 ] // Cambia los numeros que quieras
 global.mods = [] 
 global.prems = [] 
@@ -65,10 +65,10 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
+global.author = '★KHILLAZ - 𝙱𝚘𝚝★'
 
-global.wm = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.igfg = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
+global.wm = '★KHILLAZ - 𝙱𝚘𝚝★'
+global.igfg = '★KHILLAZ - 𝙱𝚘𝚝★'
 global.wait = '*⌛ _Cargando..._ ▬▬▬▭*'
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg')
